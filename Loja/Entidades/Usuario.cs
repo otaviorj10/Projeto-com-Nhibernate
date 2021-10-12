b5abbe31f010eefb2a2b34e61b3cd5f93dfa020d
Luiz Otavio <luiz_otavio71@hotmail.com>
2021-10-12 19:45:36 -0300

    iniciado projeto usando o orm NHibernate

---
+++ Loja/Entidades/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loja.Entidades
+{
+    public class Usuario
+    {
+        public virtual int Id { get; set; }
+        public virtual string Nome { get; set; }
+    }
+}
